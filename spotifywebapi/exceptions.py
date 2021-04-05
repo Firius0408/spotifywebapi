60b63083eb8e2d3b816a147c36188f5faedc0188
Brian Cheng <brian.cheng@ucla.edu>
2021-04-05 11:19:04 -0700

    Change format of StatusCodeError

--- spotifywebapi/exceptions.py
+++ spotifywebapi/exceptions.py
@@ -3,4 +3,5 @@ class SpotifyError(Exception):
 
 class StatusCodeError(SpotifyError):
     def __init__(self, status_code):
-        self.message = 'Error! API returned error code ' + status_code
+        super().__init__('Error! API returned error code ' + status_code)
+        self.status_code = status_code
