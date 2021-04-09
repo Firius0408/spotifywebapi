1627f279de4401addf8619ceb1e2ff3601930a2a
Brian Cheng <brian.cheng@ucla.edu>
2021-04-08 17:31:42 -0700

    Bump version to 1.2.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spotifywebapi",
-    version="1.2.2",
+    version="1.2.3",
     author="Brian Cheng",
     author_email="brian.cheng@ucla.edu",
     description="A simple Spotify Web API in Python",
